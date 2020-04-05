b93d14de6e0f652f2b789a2b843b8d9886c41491
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-05 22:43:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,12 +58,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each {|season, holiday| 
+  holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
-    holidays.each { |holiday, supplies|
-      puts "  #{holiday.to_s.split('_').map {|v| v.capitalize}.join(' ')}: #{supplies.join(", ")}"
-    }
-  }
+    holidays.each do |holiday, supplies|
+      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
